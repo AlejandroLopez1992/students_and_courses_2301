22446f699d1052fca136742efe32f915e53119e1
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-15 09:25:47 -0700

    Feat: create course class and initiall full? method

--- lib/course.rb
+++ lib/course.rb
@@ -1,1 +1,13 @@
+class Course
+  attr_reader :name, :capacity, :students
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
 
+  def full?
+    return false if @students.length <= @capacity
+      true
+  end
+end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -4,6 +4,11 @@ require './lib/student'
 RSpec.describe Course do
   
   it 'can initialize' do
-    
+    course = Course.new("Calculus", 2)    
+    expect(course).to be_instance_of(Course)
+    expect(course.name).to eq("Calculus")
+    expect(course.capacity).to eq(2)
+    expect(course.students).to eq([])
+    expect(course.full).to eq(false)
   end
 end

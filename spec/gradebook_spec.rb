68ab0338884de742125e2c1dac6466cb624643ce
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-15 09:43:19 -0700

    Test: add testing for add_courses method

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Gradebook do
     expect(gradebook.instructor).to eq("Farnsworth")
     expect(gradebook.courses).to eq([])
   end
+
+  it 'can add courses' do
+    gradebook = Gradebook.new("Farnsworth")
+    expect(gradebook.courses).to eq([])
+    mars_physics = Course.new("Mars Physics", 10)
+    interstellar_travel = Course.new("Interstellar Travel", 5)
+    gradebook.add_course(mars_physics)
+    gradebook.add_course(interstellar_travel)
+    expect(gradebook.courses).to eq([mars_physics, interstellar_travel])
+  end
 end

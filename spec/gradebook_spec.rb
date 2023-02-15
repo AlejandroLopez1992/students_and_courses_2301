dc300b86aa04c9a42d39f67a818190d5d18a0b72
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-15 09:51:51 -0700

    Test: add testing for list_all_students method

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe Gradebook do
     gradebook.add_course(interstellar_travel)
     expect(gradebook.courses).to eq([mars_physics, interstellar_travel])
   end
+
+  it 'can list students in courses' do
+    gradebook = Gradebook.new("Farnsworth")
+    mars_physics = Course.new("Mars Physics", 10)
+    interstellar_travel = Course.new("Interstellar Travel", 5)
+    amy_wong = Student.new({name: "Amy Wong", age: 21}) 
+    bender = Student.new({name: "Bender", age: 22})
+    mars_physics.enroll(amy_wong)
+    mars_physics.enroll(bender)
+    interstellar_travel.enroll(amy_wong)
+    interstellar_travel.enroll(bender)
+    expect(mars_physics.students).to eq([amy_wong, bender])
+    expect(interstellar_travel.students).to eq([amy_wong, bender])
+    expect(gradebook.list_all_students).to eq({mars_physics: [amy_wong, bender],
+                                               interstellar_travel: [amy_wong, bender]})
+  end
 end

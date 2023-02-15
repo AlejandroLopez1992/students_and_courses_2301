ef36236e11bc8d3091f88db0759eeeee44ea5b87
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-15 09:38:59 -0700

    Feat: add initialize method for gradebook

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe Gradebook do
     gradebook = Gradebook.new("Farnsworth")
     expect(gradebook).to be_instance_of(Gradebook)
     expect(gradebook.instructor).to eq("Farnsworth")
-    expect(gradebood.courses).to eq([])
+    expect(gradebook.courses).to eq([])
   end
 end
